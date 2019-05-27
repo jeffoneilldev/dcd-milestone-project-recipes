be82ebb3141befcefb1de316d2f85bf39e81839b
Jeff O'Neill <oneilljeff77@gmail.com>
2019-05-27 16:19:00 +0000

    added an edit recipe button and some styling

--- app.py
+++ app.py
@@ -18,6 +18,20 @@ def get_recipe():
 def add_recipe():
     return render_template("addrecipe.html",
     search_recipes=mongo.db.search_recipes.find())
+    
+@app.route('/update_recipe', methods=['POST'])
+def update_recipe():
+    recipe = mongo.db.recipe
+    recipe.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipe'))
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_searches =  mongo.db.search_recipes.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           search_recipes=all_searches)
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
